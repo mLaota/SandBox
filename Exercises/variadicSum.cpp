f63d3c9e297f91a10a4924c5b0dde7468c9b609d
mLaota <39041283+mLaota@users.noreply.github.com>
2019-05-05 12:04:53 -0400

    variadicSum

---
+++ Exercises/variadicSum.cpp
@@ -0,0 +1,24 @@
+// Compile with "g++ -std=c++17 -Wall variadicSum.cpp  -o variadicSum"
+// Construct a solution that takes a variable number of rational arguments and computes the sum
+
+#include <iostream>
+
+// base case
+template<typename T>
+T sumR(T num)
+{
+    return num;
+}
+
+// recursive case
+template<typename T, typename ... Args>
+auto sumR(T first, Args ... rest)
+{
+    return first + sumR(rest ...);
+}
+
+int main()
+{
+    double sum = sumR(2, 4.5, 5, 8, 9);
+    std::cout << "the sum is " << sum << std::endl;
+}
