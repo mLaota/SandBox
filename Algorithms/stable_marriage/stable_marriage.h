5273e68327a4b46ef368cf88838ef5924d26b92d
mLaota <39041283+mLaota@users.noreply.github.com>
2019-06-01 00:26:59 -0400

    Update Algorithms/stable_marriage/stable_marriage.h

--- Algorithms/stable_marriage/stable_marriage.h
+++ Algorithms/stable_marriage/stable_marriage.h
@@ -3,15 +3,12 @@
 
 #include <vector>
 #include <queue>
-#include <iostream>
 
 // Gale-Shapely's Stable Marriage Algorithm
 
-using namespace std;
-
-// initialize the queue of men to propose
+// initialize the std::queue of men to propose
 // O(N)
-void init_queue(int n, queue<int>& q)
+void init_queue(int n, std::queue<int>& q)
 {
     for (int i = 1; i <= n; ++i)
     {
@@ -21,7 +18,7 @@ void init_queue(int n, queue<int>& q)
 
 // for each man, record how he is ranked (j) by each woman (i)
 // O(N^2)
-void init_ranks(int n, vector<vector<int>>& rank, vector<vector<int>>& women_prefs)
+void init_ranks(int n, std::vector<std::vector<int>>& rank, std::vector<std::vector<int>>& women_prefs)
 {
     int temp;
     for(int i =0; i < n; ++i)
@@ -34,25 +31,25 @@ void init_ranks(int n, vector<vector<int>>& rank, vector<vector<int>>& women_pre
     }
 }
 
-// returns a matching represented as a vector of women such that pos i={0...N-1} in the vector
+// returns a matching represented as a std::vector of women such that pos i={0...N-1} in the std::vector
 // holds woman (i+1)'s match
-vector<int> stable_marriage(int n, vector<vector<int>> men_prefs, vector<vector<int>> women_prefs) 
+std::vector<int> stable_marriage(int n, std::vector<std::vector<int>> men_prefs, std::vector<std::vector<int>> women_prefs) 
 {
     // empty matching (to be returned)
-    vector<int> matching(n,-1);
+    std::vector<int> matching(n,-1);
     
     // represents rank of m in w's preferences
     // solves the problem of comparing ranks each iteration
     // by preprocessing ranks in O(N^2) and accessing in O(1)
-    vector<vector<int>> rank(n, vector<int>(n));
+    std::vector<std::vector<int>> rank(n, std::vector<int>(n));
     init_ranks(n,rank,women_prefs);
     
     // list of unmatched men and who they will propose to next
-    queue<int> unmatched_men;
+    std::queue<int> unmatched_men;
     init_queue(n,unmatched_men);
     
     // keeps track of what idx to propose to next for each man
-    vector<int> next_proposal(n, 0);
+    std::vector<int> next_proposal(n, 0);
     
     while (!unmatched_men.empty()) // = O(N^2) ... if all men propose to all women
     {
@@ -63,10 +60,10 @@ vector<int> stable_marriage(int n, vector<vector<int>> men_prefs, vector<vector<
         // get the index of next woman to propose to in pref list
         int idx = next_proposal[m-1];     
         
-        // if he's proposed to everyone, exit
+        // if he's proposed to everyone, exit, because the algorithm failed
         if(idx >= n)
         { 
-            exit(1);
+            exit(EXIT_FAILURE);
         }
         ++next_proposal[m-1];
 
